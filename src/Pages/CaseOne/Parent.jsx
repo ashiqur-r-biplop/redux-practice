f896e5af823a81ed25414101ee67a2e354ce15f1
Ashiqur Rahman Biplop <biplops22@gmail.com>
2024-02-23 15:34:33 +0600

    state move parent

--- src/Pages/CaseOne/Parent.jsx
+++ src/Pages/CaseOne/Parent.jsx
@@ -1,12 +1,14 @@
+import { useState } from "react";
 import ChildTwo from "../CaseTwo/ChildTwo";
 import ChildOne from "./ChildOne";
 
 const Parent = () => {
+  const [count, setCount] = useState(0);
   return (
     <div className="border-2 border-orange-700 max-w-7xl mx-auto m-10 rounded-md">
       <h1 className="m-5 text-xl">Parent</h1>
-      <ChildOne />
-      <ChildTwo />
+      <ChildOne setCount={setCount} count={count} />
+      <ChildTwo setCount={setCount} count={count} />
     </div>
   );
 };
